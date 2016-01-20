c5b08379c4249ba116ea2df8b6206a3ffab6d826
Hesham Adel <ubuntu@03f32a75121d>
2016-01-20 05:22:31 +0000

    Add 'create' method prototype to 'StringBuilder.h'

--- StringBuilder.h
+++ StringBuilder.h
@@ -9,5 +9,15 @@ typedef struct SingleLinkedList
     struct SingleLinkedList* next; 
 }StringBuilder; 
 
+// <<<<<<<<<<<<<<< METHODS PROTOTYPE >>>>>>>>>>>>>>>>>> // 
+/**
+ * create() - create a StringBuilder
+ * @param const char* first - first character into our StringBuilder 
+ * 
+ * @return StringBuilder - return a reference to our new created string 
+ */
+ StringBuilder* create(const char* firt);
+ 
+ 
 
 #endif 

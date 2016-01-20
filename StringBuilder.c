8c7728e2f62ae67507577e1bba628bf8a8b4d1d1
Hesham Adel <ubuntu@03f32a75121d>
2016-01-20 05:53:08 +0000

    create the codeblocks of StringBuilder.c

---
+++ StringBuilder.c
@@ -0,0 +1,55 @@
+/**
+ * @author       : H
+ * @date         : 20/1/16
+ * @functionality: Create a dynamic sized String
+ */
+ 
+ // librariee to include 
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include "StringBuilder.h"
+ 
+// <<<<<<<<<< Methods Implementation >>>>>>>>>>>>>>>>>//
+/**
+ * create() - create a StringBuilder
+ * @param const char* first - first character into our StringBuilder 
+ * 
+ * @return StringBuilder - return a reference to our new created string 
+ */
+ StringBuilder* create(const char* firt)
+ {
+     //TODO
+ }
+ 
+ /**
+  * append() - append character to our string 
+  * 
+  * @param char* character - the character to append to the end of our String
+  * @param const StringBuilder* head - a reference to head of our string 
+  */
+void append(char* character, const StringBuilder* head)
+{
+    //TODO
+}
+
+
+/**
+ * print() - print the content of our string
+ * 
+ * @param const StringBuilder* head - a reference to our string 
+ */
+void print(const StringBuilder* head)
+{
+    //TODO
+}
+
+
+/**
+ * destroy() - free memory allocated by the string 
+ * 
+ * @param const StringBuilder* head - a reference to the string we going to destroy
+ */
+ void destroy(const StringBuilder* head)
+ {
+     //TODO
+ }

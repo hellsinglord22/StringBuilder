69197f9d496b48443b8c5e46e9531ae98d8f9c12
Hesham Adel <ubuntu@03f32a75121d>
2016-01-20 10:07:47 +0000

    Test program

---
+++ run.c
@@ -0,0 +1,24 @@
+/**
+ * Created 4 testing purpose
+ */
+
+// libraries to include 
+#include <stdio.h>
+#include <stdlib.h>
+#include "StringBuilder.h"
+
+int main(int argc,char ** argv)
+{
+    // sanity check 
+    if (argc != 1) 
+    {
+        printf("./Usage: run <file_name>");
+        return 1; 
+    }
+
+    // Starting the testing program
+    StringBuilder* head = create('a');
+
+
+    return 0; 
+}
